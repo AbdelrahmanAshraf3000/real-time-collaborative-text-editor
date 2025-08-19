3c9b05eef3fedf9b746f94d10eeec393357a4ca3
AbdoA3000 <abdelrahman.ashraf3077@gmail.com>
2025-08-19 08:14:09 +0300

    create project

---
+++ backendcrdt/src/main/java/com/example/backendcrdt/BackendCrdtApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendcrdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCrdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCrdtApplication.class, args);
+	}
+
+}
---
+++ backendcrdt/src/test/java/com/example/backendcrdt/BackendCrdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendcrdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCrdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
